4ea4e6186d4c61f7b7101c489cef2c71b289aa3a
Sebastián Salas <tu_correo@ejemplo.com>
2025-11-18 21:09:50 -0500

    Se agrega funcionalidad para validar el cambio de valor

--- Cambio_valor_moneda.sql
+++ Cambio_valor_moneda.sql
@@ -1,1 +1,15 @@
-SELECT * FROM Moneda;
+INSERT INTO CambioMoneda (IdMoneda, Fecha, Cambio)
+VALUES (58, '2025-11-18', 4525.75)
+ON CONFLICT (IdMoneda, Fecha)
+DO UPDATE SET 
+    Cambio = EXCLUDED.Cambio;
+
+
+SELECT id, moneda, sigla
+FROM Moneda;
+
+
+SELECT *
+FROM CambioMoneda
+WHERE IdMoneda = 58
+  AND Fecha = '2025-11-18';
